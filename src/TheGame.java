5f10fdc97e91bcc51f8da58967af9f2dfe8a3ffb
dimitrow7 <v.dimitrow@gmail.com>
2016-01-18 00:12:34 +0200

    Create unique number (between 1000 and 9999)

--- src/TheGame.java
+++ src/TheGame.java
@@ -1,9 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
 
-public class TheGame {
-
+// Създаваме произволно число в границите 1000 - 9999
+public class TheGame { 
 	public static void main(String[] args) {
-		// Lets create a game :))
-
+		Random r = new Random();
+		int number = 0;
+		while (uniq(number = (r.nextInt(9000) + 1000)))
+			;
+		System.out.println(number);
 	}
 
+	// Създаваме метод, който да проверява дали числото е уникално
+	public static boolean uniq(int num) {
+		String checknum = num + "";
+		if (checknum.charAt(0) == checknum.charAt(1))
+			return false;
+		else if (checknum.charAt(1) == checknum.charAt(2))
+			return false;
+		else if (checknum.charAt(2) == checknum.charAt(3))
+			return false;
+		return true;
+	};
 }
