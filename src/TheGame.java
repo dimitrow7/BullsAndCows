b4eda1863ea014485c2d34d4a227751071f7070a
dimitrow7 <v.dimitrow@gmail.com>
2016-01-21 20:50:24 +0200

    Good working game code. Unique number generator is still not working :(

--- src/TheGame.java
+++ src/TheGame.java
@@ -1,19 +1,48 @@
 import java.util.Random;
 import java.util.Scanner;
 
-// Създаваме произволно число в границите 1000 - 9999
-public class TheGame { 
+public class TheGame {
 	public static void main(String[] args) {
 		Random r = new Random();
 		int number = 0;
+		int trynum = 0;
 		while (uniq(number = (r.nextInt(9000) + 1000)))
 			;
-		System.out.println(number);
+		String targetStr = number + "";
+		boolean game = true;
+		Scanner input = new Scanner(System.in);
+		do {
+			System.out.println("Test num: " + number);
+			int bulls = 0;
+			int cows = 0;
+			System.out.print("Guess a number: ");
+			int guess;
+			guess = input.nextInt();
+			if (guess > 9999 || guess < 1000) {
+				System.out.println("err");
+				continue;
+			}
+			trynum++;
+			String guessStr = guess + "";
+			// System.out.println(guessStr + "");
+			for (int i = 0; i < 4; i++) {
+				if (guessStr.charAt(i) == targetStr.charAt(i)) {
+					bulls++;
+				} else if (targetStr.contains(guessStr.charAt(i) + "")) {
+					cows++;
+				}
+			}
+			if (bulls == 4) {
+				game = false;
+			} else {
+				System.out.println(cows + " Cows and " + bulls + " Bulls.");
+			}
+		} while (game);
+		System.out.println("You won after " + trynum + " guesses!");
 	}
 
-	// Създаваме метод, който да проверява дали числото е уникално
-	public static boolean uniq(int num) {
-		String checknum = num + "";
+	public static boolean uniq(int number) {
+		String checknum = number + "";
 		if (checknum.charAt(0) == checknum.charAt(1))
 			return false;
 		else if (checknum.charAt(1) == checknum.charAt(2))
